571310c173f4b5dbd8bb38c366de728f655cbe77
ravi <rvishrma007@gmail.com>
2020-09-24 04:01:05 +0530

    more responsive

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var connection = mysql.createConnection({
         console.log('connected');
   });
 
-app.listen(process.env.PORT || 3000, (err)=>{
+app.listen(process.env.PORT|| 3000, (err)=>{
     if(err)
     {
         console.log(err);
