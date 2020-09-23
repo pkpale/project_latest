002298b01670d80f6c81e18d6a53c23680d92c39
ravi <rvishrma007@gmail.com>
2020-09-23 21:21:22 +0530

    first deployment

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.listen(3000, (err)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else{
+        console.log('listening');
+    }
+});
+
+app.set('view engine' , 'ejs');
+app.use(express.static('public'));
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+app.get('/contact-us', (req, res)=>{
+    res.render('contact');
+});
