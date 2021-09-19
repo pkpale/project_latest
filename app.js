8eb9261234c94cede7acfe7c09721b0af190eca7
Praveen Kumar <pk349368@gmail.com>
2021-09-18 23:26:08 -0700

    after ravi page for sandvine

--- app.js
+++ app.js
@@ -16,14 +16,21 @@ let db = new sqlite3.Database('./database.db', (err) => {
   });
 
 
-  // creating table student_profile
+// db.run('drop table employee;')
+//   creating table student_profile
 //   db.run('create table student_profile(emailid varchar(20) primary key, fname varchar(20), lname varchar(20), tel varchar(10), location varchar(20), linkedin_id varchar(50),  skype_id varchar(50),    graduation_year varchar(4),   gschool varchar(20),    gcgpa varchar(10),    gbranch varchar(20),    xiischool varchar(20),  xiicgpa varchar(10),     xschool varchar(20),    xcgpa varchar(10) );');
 // db.run('drop table student');
-// creating table student 
-//   db.run('create table student(emailid varchar(20) primary key, fname varchar(20), lname varchar(20), rollno varchar(20), pass varchar(64), tel char(10))')
-  
-  
+// creating table employee 
+//   db.run('create table employee(emailid varchar(20) primary key, fname varchar(20), lname varchar(20), emp_id varchar(8), pass varchar(64), tel char(10), cafeteria_seat varchar(3) DEFAULT null);')
+//   console.log('print table')
+//   console.log(db.run('select * from employee;'));
 
+// db.run(`insert into employee values( 'ravi@sandvine.com' , 'ravi', 'sharma', '1234',  'hsdjs', '78273827382', 'null');`)
+db.all('select * from employee;', (err, data) => {
+    if(err)
+        console.log(err);
+    else console.log(data);
+});
 
 app.listen( process.env.PORT || 3000, (err)=>{
     if(err)
@@ -48,7 +55,7 @@ app.post('/check', (req,res)=>{
         email:true,
         pass:false
     }
-    db.get(`select * from student where emailid = '${inp.email}'`, (err, data)=>{
+    db.get(`select * from employee where emailid = '${inp.email}'`, (err, data)=>{
         if(data)
             data.pass = crypto.createHash('sha256').update(data.pass).digest('hex');
 
@@ -64,13 +71,14 @@ app.post('/',(req,res)=>{
     console.log(data.email);
     console.log(data.pass);
     data.pass = crypto.createHash('sha256').update(data.pass).digest('hex');
-    db.run(`insert into student values( '${data.email}' , '${data.fname}', '${data.lname}', '${data.rollno}',  '${data.pass}', '${data.tel}');`, (err)=>{
+    db.run(`insert into employee values( '${data.email}' , '${data.fname}', '${data.lname}', '${data.emp_id}',  '${data.pass}', '${data.tel}', 'null');`, (err)=>{
         if(err){
             console.log(err);   
         }    
-        else
+        else{
             console.log('record added');
-        res.json({redirect:'/login'});
+            res.json({redirect:'/login'});
+        }
     });
 });
 app.get('/about', (req, res)=>{
@@ -80,71 +88,73 @@ app.get('/login',(req,res)=>{
     res.render('login');
 });
 
-app.get('/edit/:id', (req,res)=>{
-    const id = req.params.id;
-    db.run(`delete from student_profile where emailid = '${id}'`, (err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-                db.get(`select * from student where emailid = '${id}'`, (err, data1)=>{
-                    
-                        res.render('pro',{data:data1});
-                    
-                });
-           
-        }
-    });
-});
 app.get('/contact-us', (req, res)=>{
     res.render('contact');
 });
 
+app.get('/user/cafeteria/:id', (req, res) => {
+    const id = req.params.id;
+    db.get(`select * from employee where emailid = '${id}'`, (err, data)=>{
+        db.all(`select cafeteria_seat from employee where emailid != '${id}' and cafeteria_seat != 'null';`, (err, data1) => {
+            if (err)
+                console.log(err);
+            else {
+               console.log(data1.length)
+               res.render('cafeteria', {data:data, data1:data1});
+            }
+        });
+    });
+});
 app.get('/user/:id',(req,res)=>{
     console.log(req.url);
     const id = req.params.id;
     console.log('india');
-    
-    db.get(`select * from student_profile where emailid = "${id}"`,(err, data)=>{
-
-        db.get(`select * from student where emailid = '${id}'`, (err, data1)=>{
-            if(!data)
-            {
-                res.render('pro',{data:data1});
-            }
-            else{
-                res.render('user',{data:data, data1:data1});
-            }
-            
-            console.log(data);
+    // let data1, data;
+        db.get(`select * from employee where emailid = '${id}'`, (err, data)=>{
+            // db.get(`select cafeteria_seat from employee where emailid!='${id}';`, (err, data1) => {
+                if (err)
+                    console.log(err);
+                else 
+                //    console.log(data, data1," ravi")
+                   res.render('home', {data:data});
+            // });
         });
-    });
+        
+        // console.log(data, data1, 'ravi')
+        // res.render('cafeteria',{data:data, data1:data1});  
 });
 
-
+app.post('/user/save/:id', (req, res) => {
+    var id = req.params.id;
+    var seat_no = (req.headers.seat_no);
+    console.log(db.get('select * from employee'));
+    console.log(seat_no);
+    db.run(`update employee set cafeteria_seat = '${seat_no}' where emailid='${id}';`, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(db.run('select * from employee;'));
+    res.json({redirect:'../'});
+});
 app.post('/user/:id', (req,res)=>{
-    
-
-    console.log('2020');
     console.log(req.url);
         var pass = (req.headers.password);
         var id = req.params.id;
-        console.log('****');
         var obj =  {email:true, password:false, redirect:`/user/${id}`};
         console.log(obj.redirect);
-        var sql = `select * from student where emailid = '${id}';`;
+        var sql = `select * from employee where emailid = '${id}';`;
         db.get(sql, (err, data)=>{        
-    //    res.setHeader('content-type')
-           
+        //    res.setHeader('content-type')
             pass = crypto.createHash('sha256').update(pass).digest('hex');
 
             console.log(sql);
-    console.log(data);
+            console.log(data);
             if(!data)
                 obj.email = false;
             else if(data.pass == pass)
                 obj.password=true;
-        res.json(obj);
+            res.json(obj);
         // res.end();
         });
        
@@ -153,17 +163,11 @@ app.post('/:id', (req,res)=>{
     const id = req.params.id;
     console.log(req.body);
     const form = req.body;
-    db.get(`select * from student where emailid = '${id}'`, (err, data)=>{        
-        data = data;
-        db.run(`insert into student_profile values( '${data.emailid}' , '${data.fname}', '${data.lname}', '${form.tel}','${form.location}','${form.linkedin_id}', '${form.skype_id}','${form.graduation_year}','${form.gschool}','${form.gcgpa}','${form.gbranch}', '${form.xiischool}','${form.xiicgpa}','${form.xschool}','${form.xcgpa}');`, (err)=>{
-            if(err){
-                console.log(err);   
-                console.log(data);
-            }    
-            else
-                console.log('record added');
+    db.get(`select * from employee where emailid = '${id}'`, (err, data)=>{        
+        if(err)
+            console.log(err);
+        else
             res.redirect(`/user/${id}`);
-        });
 
     });
 });
@@ -178,7 +182,7 @@ app.post('/edit/:id', (req,res) =>{
         res.json('enter correct emailid');
     }
     else
-        db.get(`select * from student where emailid = '${id}'`, (err, data1)=>{
+        db.get(`select * from employee where emailid = '${id}'`, (err, data1)=>{
         
             if(err)
                 console.log(err);
